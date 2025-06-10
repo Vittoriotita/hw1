f85c90025a16b96f92e1bbbb6ed55ea446e1f5d9
Vittorio Tita <vittorio.tita@gmail.com>
2025-06-10 21:43:08 +0200

    Update youtube_api.php
    Ho tolto la key.

--- youtube_api.php
+++ youtube_api.php
@@ -2,8 +2,8 @@
     require_once 'check_Session.php';
 
     header("Content-Type: application/json");
-
-    $apiKey = "AIzaSyCEv7LPwlvaYycV4UiBSr3O_n4jBYj-BKQ";
+    
+    $apiKey = "";
     $channelID = "UCE_M8A5yxnLfW0KghEeajjw";
     $uploadsID = "UUE_M8A5yxnLfW0KghEeajjw";
     $url = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=UUE_M8A5yxnLfW0KghEeajjw&key=$apiKey";
